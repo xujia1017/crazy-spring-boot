32a951e1dd17bdd826ddd6072f40a31c1a731f11
XuJia <859205795@qq.com>
2021-11-17 11:38:47 +0800

    init

---
+++ 03/3.6/Annotation/src/main/java/org/crazyit/app/service/ItemService.java
@@ -0,0 +1,15 @@
+package org.crazyit.app.service;
+
+import org.crazyit.app.domain.Item;
+
+import java.util.Collection;
+
+public interface ItemService {
+    Collection<Item> list();
+
+    Item getItemById(Integer id);
+
+    Item createOrUpdate(Item item);
+
+    Item delete(Integer id);
+}
---
+++ 04/4.1/Restful_XML/src/main/java/org/crazyit/app/service/BookService.java
@@ -0,0 +1,11 @@
+package org.crazyit.app.service;
+
+import org.crazyit.app.domain.Book;
+
+import java.util.Collection;
+
+public interface BookService {
+    Collection<Book> list();
+
+    Book createOrUpdate(Book book);
+}
